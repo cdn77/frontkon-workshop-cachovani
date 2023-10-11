45d7e94d8f02ffc980caff8fc819cc521250294b
= <=>
2023-10-11 20:34:20 +0200

    feat: add a possible solution

--- server/getHeaders.js
+++ server/getHeaders.js
@@ -29,11 +29,26 @@ export const getHeaders = (path) => {
   const twoYears = 2 * 365 * 24 * 60 * 60;
   const fiveMinutes = 5 * 60;
 
-  if (false) {
+  if (path.startsWith("/assets/")) {
+    // cache immutable assets for 2 years
     return {
-      "Cache-Control": "...", // TODO
+      "Cache-Control": `public, max-age=${twoYears}, immutable`,
+    };
+  } else if (/\.(svg|png)$/.test(path)) {
+    // cache mutable images for 5 minutes (this means that users might see old images for 5 minutes before the cache expires)
+    return {
+      "Cache-Control": `public, max-age=${fiveMinutes}`,
+    };
+  } else if (path.startsWith("/fonts/")) {
+    // fonts rarely change, we can cache them for a long time even though they are mutable
+    // if we really need to update the font, we can give the new version a different name
+    return {
+      "Cache-Control": `public, max-age=${twoYears}`,
     };
   }
 
+  // Since this is a public, static site, we can mark other files as public.
+  // This is not always the case. Be aware of customized or otherwise user-specific content like
+  // pages with forms, pages that require logout, or websites that render stuff differently based on the request
   return { "Cache-Control": "public, max-age=0, must-revalidate" };
 };
