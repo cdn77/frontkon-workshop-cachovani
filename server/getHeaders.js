708d184b0e5cc597322aac3405b3d5c3bf1e3018
= <=>
2023-10-12 07:48:40 +0200

    feat: add a possible solution using s-maxage

--- server/getHeaders.js
+++ server/getHeaders.js
@@ -10,13 +10,13 @@
  *    - private, max-age=0                  - for user-specific responses - server side rendered HTML, form responses, etc - anything that should not ever be cached
  *
  * Important directives (explanations are simplified):
- *    - max-age=N               - response remains fresh (= cached in the browser) for N seconds
- *    - no-cache                - treat response as stale (= refetch on next request)
- *    - must-revalidate         - response must be refetched when stale (= do not use stale response even offline)
- *    - public                  - this response looks the same for all visitors
- *    - private                 - this response is private to this specific visitor
- *    - immutable               - the content will not change while fresh
- *    - stale-while-revalidate  - server stale content and refetch in the background - the next load will serve the newly fetched content from the updated cache
+ *    - max-age=N                 - response remains fresh (= cached in the browser) for N seconds
+ *    - no-cache                  - treat response as stale (= refetch on next request)
+ *    - must-revalidate           - response must be refetched when stale (= do not use stale response even offline)
+ *    - public                    - this response looks the same for all visitors
+ *    - private                   - this response is private to this specific visitor
+ *    - immutable                 - the content will not change while fresh
+ *    - stale-while-revalidate=N  - server stale content (for up to N seconds) and refetch in the background - the next load will serve the newly fetched content from the updated cache
  */
 
 /**
@@ -37,7 +37,7 @@ export const getHeaders = (path) => {
   } else if (/\.(svg|png)$/.test(path)) {
     // cache mutable images for 5 minutes (this means that users might see old images for 5 minutes before the cache expires)
     return {
-      "Cache-Control": `public, max-age=${fiveMinutes}`,
+      "Cache-Control": `public, s-maxage=${twoYears}, max-age=${fiveMinutes}`,
     };
   } else if (path.startsWith("/fonts/")) {
     // fonts rarely change, we can cache them for a long time even though they are mutable
@@ -50,5 +50,7 @@ export const getHeaders = (path) => {
   // Since this is a public, static site, we can mark other files as public.
   // This is not always the case. Be aware of customized or otherwise user-specific content like
   // pages with forms, pages that require logout, or websites that render stuff differently based on the request
-  return { "Cache-Control": "public, max-age=0, must-revalidate" };
+  return {
+    "Cache-Control": `public, s-maxage=${twoYears}, max-age=5, must-revalidate`,
+  };
 };
